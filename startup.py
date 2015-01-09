804d8ab4c7c1a1a17b33fc1b1bd9581a6d1660b0
Rob Berry <rob@lostpropertyhq.com>
2015-01-09 15:40:22 +0000

    Remove unused functions

--- startup.py
+++ startup.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
 import os
-import sys
 import json
 import os.path
-import datetime
 import functools
 import subprocess
 
@@ -54,17 +52,6 @@ def resource_may_exist(fn):
             response = "Created resource '{}'".format(resource)
         return response
     return inner
-
-
-def debug(cmdfn):
-    def inner(*args, **kwargs):
-        return u'echo ' + cmdfn(*args, **kwargs)
-    return inner
-
-
-def client_token(delim=6):
-    date = datetime.datetime.utcnow().strftime('%Y-%m-%d')
-    return u'{user}-{delim}-{date}'.format(user=os.environ['USER'], date=date, delim=delim)
 
 
 def underscores_to_dashes(keys):
@@ -120,49 +107,12 @@ def create_auto_scaling_group(launch_config_name, group_name, subnet_id):
     return call('autoscaling', 'create-auto-scaling-group', **args)
 
 
-def run_instances():
-    args = {
-        'image_id': ami_id,
-        'security_groups': security_groups,
-        'instance_type': instance_type,
-        'user_data': user_data,
-        'instance_initiated_shutdown_behavior': 'stop',
-        'key_name': key_name,
-        'iam_instance_profile': KeyValue(Name='scheduler'),
-        'placement': KeyValue(AvailabilityZone=zone),
-        'count': 1,
-        'output': 'text',
-        'query': 'Instances[*].InstanceId',
-        'client_token': client_token(),
-    }
-    return call('ec2', 'run-instances', **args)
-
-
-def describe_instances(instance_id):
-    args = {
-        'instance_ids': instance_id,
-    }
-    response = call('ec2', 'describe-instances', **args)
-    try:
-        return json.loads(response)
-    except ValueError:
-        sys.exit("Describing instance failed: {!r}".format(response))
-
-
 def suspend_processes(group_name):
     args = {
         'auto_scaling_group_name': group_name,
         'scaling_processes': 'ReplaceUnhealthy',
     }
     return call('autoscaling', 'suspend-processes', **args)
-
-
-def attach_instances(group_name, instance_id):
-    args = {
-        'auto_scaling_group_name': group_name,
-        'instance_ids': instance_id,
-    }
-    return call('autoscaling', 'attach-instances', **args)
 
 
 def set_group_size(group_name, action_name, size, recurrence):
@@ -183,44 +133,13 @@ def describe_scheduled_actions(group_name):
     return call('autoscaling', 'describe-scheduled-actions', **args)
 
 
-def create_tags(instance_id, key='Name', value='scheduler'):
-    args = {'resources': instance_id,
-            'tags': 'Key={k},Value={v}'.format(k=key, v=value)}
-    return call('ec2', 'create-tags', **args)
-
-
-def get_ip_address(description):
-    instance = description['Reservations'][0]['Instances'][0]
-    try:
-        retval = instance['PublicIpAddress']
-    except KeyError:
-        state_reason = instance.pop('StateReason', '')
-        retval = 'Cannot determine ip: {State!r} {StateReason!r}'.format(StateReason=state_reason, **instance)
-        sys.exit(retval)
-    return retval
-
-
-def get_subnet_id(description):
-    return description['Reservations'][0]['Instances'][0]['NetworkInterfaces'][0]['SubnetId']
-
-
 def main():
 
     # FIXME create an ami from the provisioned instance. This will save us
     # from installing pip, awscli, etc all the time
-
-    # instance_id = run_instances()
-    # description = describe_instances(instance_id)
-    # ip = get_ip_address(description)
-    # subnet_id = get_subnet_id(description)
-    # print("Instance {} running on {}".format(instance_id, ip))
-    # create_tags(instance_id)
-    # print("Tagging instance {}".format(instance_id))
     print(create_launch_configuration(launch_config))
     print(create_auto_scaling_group(launch_config, auto_scale_group, subnet_id='N/a'))
     suspend_processes(auto_scale_group)
-    # attach_instances(auto_scale_group, instance_id)
-    # print("Attached instance to auto-scaling group")
     set_group_size(auto_scale_group, Quoted("Begin downtime check"), size=1,
                    recurrence=Quoted("0,15,30,45 * * * *"))
     set_group_size(auto_scale_group, Quoted("Finish downtime check"), size=0,

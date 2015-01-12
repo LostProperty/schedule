846ee7c2d8414c9e8b4170c1e270c767f4b6d4cc
Rob Berry <rob@lostpropertyhq.com>
2015-01-12 16:02:14 +0000

    Change userdata script
    - The report email now includes the current status of running instances
    - The instance shuts itself down after completing the check

--- schedule/scheduler.sh
+++ schedule/scheduler.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -x
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
+# Get some information about the running instance
+instance_id=$(wget -qO- instance-data/latest/meta-data/instance-id)
+public_ip=$(wget -qO- instance-data/latest/meta-data/public-ipv4)
+zone=$(wget -qO- instance-data/latest/meta-data/placement/availability-zone)
+region=$(expr match $zone '\(.*\).')
 
 # We have to install pip so we can install awscli
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -9,19 +14,17 @@ pip install --upgrade awscli
 pip install https://github.com/LostProperty/schedule/zipball/develop
 
 # tag this instance
-aws ec2 create-tags --resources $(ec2metadata --instance-id) --tags Key=Name,Value=scheduler --region eu-west-1
+aws ec2 create-tags --resources $(ec2metadata --instance-id) --tags Key=Name,Value=scheduler --region $region
+
+# check for running instances
+python -m schedule.app check --region $region
 
 EMAIL=rob@lostpropertyhq.com
 
 # Upgrade and install Postfix so we can send a sample email
 export DEBIAN_FRONTEND=noninteractive
 apt-get update && apt-get install -y postfix
-
-# Get some information about the running instance
-instance_id=$(wget -qO- instance-data/latest/meta-data/instance-id)
-public_ip=$(wget -qO- instance-data/latest/meta-data/public-ipv4)
-zone=$(wget -qO- instance-data/latest/meta-data/placement/availability-zone)
-region=$(expr match $zone '\(.*\).')
+status=$(aws ec2 describe-instances --query "Reservations[*].Instances[0].[Tags,State]" --region $region --filter Name=tag-key,Values=Scheduled)
 uptime=$(uptime)
 
 # Send status email
@@ -44,16 +47,18 @@ job using a command like:
 
   ec2-describe-instances --region $region $instance_id
 
-EOM
 
-# check for running instances
-python -m schedule.app check --region eu-west-1
+The result of running the schedule check is:
+
+  $status
+
+EOM
 
 # Give the email some time to be queued and delivered
 sleep 300 # 5 minutes
 
 # This will stop the EBS boot instance, stopping the hourly charges.
 # Have Auto Scaling terminate it, stopping the storage charges.
-# shutdown -h now
+shutdown -h now
 
 exit 0
